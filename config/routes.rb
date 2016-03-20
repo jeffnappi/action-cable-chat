eab0f4c9435c7e974fb63644e1baa0879ca86ade
Jeff Nappi <jeff@clearvoice.com>
2016-03-19 20:02:21 -0700

    Add rooms/message views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'rooms/show'
+  root to: 'rooms#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
